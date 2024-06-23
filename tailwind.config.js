aeb3dfcfd8183618cc63041ff49217d92961fa84
leslylulu <dearlulu1995@gmail.com>
2024-06-23 17:31:08 -0400

    add models

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      serif: ["Playfair Display", ...defaultTheme.fontFamily.sans],
+      sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+    },
+  },
+  plugins: [],
+};
